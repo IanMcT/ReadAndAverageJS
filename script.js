0a8e07910903e5a77ea8e960f1b35163ee33b8a9
IanMcT <ian.mctavish@tldsb.on.ca>
2022-10-25 18:04:18 +0000

    Averaged the numbers

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+var total = 0; 
+var counter = 0;
 function readfile(){
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function(){
     var fileContents = this.responseText;
     alert(fileContents);
     var numbers = fileContents.split("\n");
+    for(var i = 0;i < numbers.length; i++){
+      total += parseInt(numbers[i]);
+      counter++;
+    }
     alert(numbers);
-    alert(numbers[0]);
+    var average = total/counter;
+    alert(average + " average");
   }
   xhttp.open("GET","numbers.txt");
   xhttp.send();
